d80951b8825bace7aacf4199058d4d69cd3c4e11
holo <solleza@ualberta.ca>
2018-09-20 18:16:46 -0600

    lonelyTwitter cloned

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -37,10 +37,21 @@ public class LonelyTwitterActivity extends Activity {
 		saveButton.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
-				setResult(RESULT_OK);
-				String text = bodyText.getText().toString();
-				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
+				public void onClick(View v) {
+					Cat cat1 = new Cat();
+					cat1.setType("I am a cat");
+					cat1.setFood("I love fish and rice");
+
+					DomesticDuck domesticDuck1 = new DomesticDuck();
+					domesticDuck1.setType("I am a domestic duck");
+					domesticDuck1.setFood("I love fish");
+
+					Log.d(tag:"cmput-301", cat1.getType());
+					Log.d(tag:"cmput-301", cat1.getFood());
+					Log.d(tag:"cmput-301", domesticDuck1.getType());
+					Log.d(tag:"cmput-301", domesticDuck1.getFood());
+
+				}
 
 			}
 		});
