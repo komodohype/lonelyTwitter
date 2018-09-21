b8a03f010215d818f1965dd30ce0d7a1a4b08540
holo <solleza@ualberta.ca>
2018-09-20 18:03:45 -0600

    lonelyTwitter cloned

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Animal.java
@@ -0,0 +1,22 @@
+package ca.ualberta.cs.lonelytwitter;
+
+public class Animal {
+    private String type;
+    private String food;
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getFood() {
+        return this.food;
+    }
+}
